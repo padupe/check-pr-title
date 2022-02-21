8df38aa0f845f1fe1f44516c5d3294e86426a9ab
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2022-02-21 15:43:45 -0300

    feat(index): update title PR validation.

--- dist/index.js
+++ dist/index.js
@@ -8477,25 +8477,19 @@ async function run() {
 
         let PRDefault = /[a-z]+\([A-Z]+-\d+\):.*/
         let PRHotFix = /(hotfix)+\:.*/
-        
-        console.log("Passo 2")
 
         let validateTitle = PRDefault.test(titlePR)
         let validateHotFix = PRHotFix.test(titlePR)
 
-        console.log("Passo 3 - Validação via IF")
-
         if ( validateTitle == false && validateHotFix == false) {
             core.setFailed('ERRO. Título da Pull Request não está no padrão.\n"tipoPR(IDJIRA): Descrição." ou "hotfix: descrição."')
         } else if (validateTitle == true && validateHotFix == false) {
-            core.setOutput('Título permite que a GMUD pode ser criada.')
+            console.log('Título permite que a GMUD seja criada.')
             return true
         } else if (validateTitle == false && validateHotFix == true) {
-            core.setOutput('Hotfix, não será criada a GMUD.')
+            console.log('Hotfix, não será criada a GMUD.')
             return true
         }
-
-        console.log("Terminou")
 
     } catch (e) {
         core.setFailed(`Essa ação só será executada em uma Pull Request.\nERRO: ${e}.`)
